f726e26e6b77b0d009fd4483db8a0e9ba475994d
skeown13 <code@keown.net>
2020-05-19 12:23:44 -0700

    created an images.js file as a file where all the image thumbnails will render when the webpage is opened. added the function for the beginner clues images and called it for each image

---
+++ assets/javascript/images.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+
+  function beginnerCluesImg(imgId, imgObjId) {
+    $(`#${imgId.id}`).attr("src", `./assets/images/beginnerClues/${imgObjId}.png`)
+  }
+  
+  
+  // Beginner Clues Image Thumbnails being Rendered on the Screen
+
+  beginnerCluesImg(beginner_runeScimitarOrnamentKit_guthix, beginnerClues.runeScimitarOrnamentKit_guthix.item_id)
+
+  beginnerCluesImg(beginner_runeScimitarOrnamentKit_saradomin, beginnerClues.runeScimitarOrnamentKit_saradomin.item_id)
+
+  beginnerCluesImg(beginner_runeScimitarOrnamentKit_zamorak, beginnerClues.runeScimitarOrnamentKit_zamorak.item_id)
+
+  beginnerCluesImg(beginner_bearFeet, beginnerClues.bearFeet.item_id)
+
+  beginnerCluesImg(beginner_demonFeet, beginnerClues.demonFeet.item_id)
+
+  beginnerCluesImg(beginner_frogSlippers, beginnerClues.frogSlippers.item_id)
+
+  beginnerCluesImg(beginner_moleSlippers, beginnerClues.moleSlippers.item_id)
+
+  beginnerCluesImg(beginner_jesterCape, beginnerClues.jesterCape.item_id)
+
+  beginnerCluesImg(beginner_monksRobeTop_t, beginnerClues.monksRobeTop_t.item_id)
+
+  beginnerCluesImg(beginner_monksRobe_t, beginnerClues.monksRobe_t.item_id)
+
+  beginnerCluesImg(beginner_sandwichLadyHat, beginnerClues.sandwichLadyHat.item_id)
+
+  beginnerCluesImg(beginner_sandwichLadyTop, beginnerClues.sandwichLadyTop.item_id)
+
+  beginnerCluesImg(beginner_sandwichLadyBottom, beginnerClues.sandwichLadyBottom.item_id)
+
+  beginnerCluesImg(beginner_amuletOfDefense_t, beginnerClues.amuletOfDefense_t.item_id)
+
+  beginnerCluesImg(beginner_shoulderParrot, beginnerClues.shoulderParrot.item_id)
+
+  beginnerCluesImg(beginner_blackPickaxe, beginnerClues.blackPickaxe.item_id)
+
+
+
+
+
+
+})
